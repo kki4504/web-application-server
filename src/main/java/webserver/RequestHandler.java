419e6ca5b5e24f7f213b8bb3414d70c3acb17119
kki4504 <rhkddlf9876@gmail.com>
2023-01-27 12:01:37 +0900

    EDIT) RequestHandler - 3.4.3.1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +23,20 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World My Name is Pang".getBytes();
+//            byte[] body = "Hello World My Name is Pang".getBytes();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                return ;
+            }
+            String url = line.split(" ")[1];
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            while(!"".equals(line)) {
+                log.debug(line);
+                line = bufferedReader.readLine();
+            }
+            
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
