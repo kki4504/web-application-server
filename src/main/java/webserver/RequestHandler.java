006641b55ece6f57904612cb7293f62e4f7f257d
kki4504 <rhkddlf9876@gmail.com>
2023-02-22 18:42:39 +0900

    EDIT) RequestHandler - 3.4.3.2

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,12 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,24 +24,34 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World My Name is Pang".getBytes();
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = bufferedReader.readLine();
             if (line == null) {
                 return ;
             }
-            String url = line.split(" ")[1];
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            while(!"".equals(line)) {
-                log.debug(line);
-                line = bufferedReader.readLine();
+            String url = HttpRequestUtils.getUrl(line);
+            if (url.startsWith("/user/create")) {
+                int index = url.indexOf("?");
+                String queryString = url.substring(index + 1);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+                User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+                log.debug("User {}", user);
+
+                url = "/index.html";
             }
-            
+//            while(!"".equals(line)) {
+//                log.debug("header : {}", line);
+//                line = bufferedReader.readLine();
+//            }
+
+            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            DataOutputStream dos = new DataOutputStream(out);
+
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
